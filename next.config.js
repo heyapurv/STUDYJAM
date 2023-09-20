4c3248ece686bda0a035fd59a57ff6709ff2b7a0
STRiiXER <127283772+striixervro@users.noreply.github.com>
2023-09-20 12:38:51 +0530

    new

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports =  { nextConfig,
+    // Other configuration options
+    target: 'serverless', // This is required for static export
+    exportPathMap: async function () {
+      return {
+        '/': { page: '/' },
+      };
+    },
+    // Add any other necessary configuration
+  };
