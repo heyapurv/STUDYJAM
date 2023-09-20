61a5eb4c3ef79e77c47cf7e3ac3aedb3b876c96b
STRiiXER <127283772+striixervro@users.noreply.github.com>
2023-09-20 12:54:01 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    // Add the following line to enable static HTML export
+    exportPathMap: function () {
+      return {
+        '/': { page: '/' }, // Add more paths as needed
+      };
+    },
+  };
+  
+  module.exports = nextConfig;
+  
